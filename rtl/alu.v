05d6432d986b065fe735ad3ec3f8fde0fcc50422
BraveSeeker <202419330@qq.com>
2022-12-29 18:13:37 +0800

    更新52条指令（一部分）

--- rtl/alu.v
+++ rtl/alu.v
@@ -19,36 +19,69 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+`include "defines.vh"
 module alu(
-	input wire[31:0] a,b,
-	input wire[2:0] op,
-	output reg[31:0] y,
+	input wire [31:0] alu_num1,
+	input wire [31:0] alu_num2,
+	input wire [4:0] sa,
+	input wire [7:0]alucontrol;
+
+	output reg [31:0] alu_out,
 	output reg overflow,
 	output wire zero
     );
-
-	wire[31:0] s,bout;
-	assign bout = op[2] ? ~b : b;
-	assign s = a + bout + op[2];
 	always @(*) begin
-		case (op[1:0])
-			2'b00: y <= a & bout;
-			2'b01: y <= a | bout;
-			2'b10: y <= s;
-			2'b11: y <= s[31];
-			default : y <= 32'b0;
-		endcase	
-	end
-	assign zero = (y == 32'b0);
+		case(alucontrol)
+			// 逻辑运算
+			`EXE_AND_OP	:	alu_out <= alu_num1 & alu_num2;
+			`EXE_OR_OP	:	alu_out <= alu_num1 | alu_num2;
+			`EXE_XOR_OP	:	alu_out <= alu_num1 ^ alu_num2;
+			`EXE_NOR_OP	:	alu_out <= ~(alu_num1 | alu_num2);
+			// 逻辑立即数运算
+			`EXE_ANDI_OP:	alu_out <= alu_num1 & {{16{1'b0}}, alu_num2[15:0]};
+			`EXE_ORI_OP:	alu_out <= alu_num1 | {{16{1'b0}}, alu_num2[15:0]};
+			`EXE_XORI_OP:	alu_out <= alu_num1 ^ {{16{1'b0}}, alu_num2[15:0]};
+			`EXE_LUI_OP:	alu_out <= {alu_num2[15:0], {16{1'b0}}};
+			// 移位指令
+			`EXE_SLL_OP: 	alu_out <= alu_num2 << sa;
+			`EXE_SRL_OP: 	alu_out <= alu_num2 >> sa;
+			`EXE_SRA_OP: 	alu_out <= $signed(alu_num2) >>> sa;
+			`EXE_SLLV_OP: 	alu_out <= alu_num2 << alu_num1[4:0];
+			`EXE_SRLV_OP: 	alu_out <= alu_num2 >> alu_num1[4:0];
+			`EXE_SRAV_OP: 	alu_out <= $signed(alu_num2) >>> alu_num1[4:0];
+			
+			// 数据移动指令
+			// `EXE_MFHI_OP:	alu_out <= 
+			// `EXE_MFLO_OP:	alu_out <= 
+			// `EXE_MTHI_OP:	alu_out <= 
+			// `EXE_MTLO_OP:	alu_out <= 
 
-	always @(*) begin
-		case (op[2:1])
-			2'b01:overflow <= a[31] & b[31] & ~s[31] |
-							~a[31] & ~b[31] & s[31];
-			2'b11:overflow <= ~a[31] & b[31] & s[31] |
-							a[31] & ~b[31] & ~s[31];
-			default : overflow <= 1'b0;
-		endcase	
+			// 算术运算指令
+			`EXE_ADD_OP:	alu_out <= alu_num1 + alu_num2;
+			`EXE_ADDU_OP:	alu_out <= alu_num1 + alu_num2;
+			`EXE_SUB_OP:	alu_out <= alu_num1 - alu_num2;
+			`EXE_SUBU_OP:	alu_out <= alu_num1 - alu_num2;
+			`EXE_SLT_OP:	alu_out <= $signed(alu_num1) < $signed(alu_num2);
+			`EXE_SLTU_OP:	alu_out <= alu_num1 < alu_num2;
+            `EXE_ADDI_OP:	alu_out <= alu_num1 + alu_num2;
+            `EXE_ADDIU_OP:	alu_out <= alu_num1 + alu_num2;
+            `EXE_SLTI_OP:	alu_out <= $signed(alu_num1) < $signed(alu_num2);
+            `EXE_SLTIU_OP:	alu_out <= alu_num1 < alu_num2;
+			
+			// 分支跳转指令
+			`EXE_J_OP:		alu_out <= alu_num1 + alu_num2;
+			// `EXE_JR_OP:		alu_out <= alu_num1 + alu_num2;
+			// `EXE_JAL_OP:		alu_out <= alu_num1 + alu_num2;
+			// `EXE_JALR_OP:	alu_out <= alu_num1 + alu_num2;
+			
+
+
+			// 访存指令
+
+			// 内陷指令
+
+			// 特权指令
+
+
 	end
 endmodule
--- rtl/aludec.v
+++ rtl/aludec.v
@@ -19,24 +19,65 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+`include "defines.vh"
 module aludec(
-	input wire[5:0] funct,
-	input wire[1:0] aluop,
-	output reg[2:0] alucontrol
+	input wire [5:0] funct,
+	input wire [5:0] op,
+    input wire [4:0] rs,
+    input wire [4:0] rt,
+	output reg [7:0] alucontrol
     );
 	always @(*) begin
-		case (aluop)
-			2'b00: alucontrol <= 3'b010;//add (for lw/sw/addi)
-			2'b01: alucontrol <= 3'b110;//sub (for beq)
-			default : case (funct)
-				6'b100000:alucontrol <= 3'b010; //add
-				6'b100010:alucontrol <= 3'b110; //sub
-				6'b100100:alucontrol <= 3'b000; //and
-				6'b100101:alucontrol <= 3'b001; //or
-				6'b101010:alucontrol <= 3'b111; //slt
-				default:  alucontrol <= 3'b000;
+		case (op)
+			// R-type
+			`EXE_NOP:case (funct)
+				// 逻辑运算指令
+				`EXE_AND	:alucontrol <= `EXE_AND_OP;
+				`EXE_OR		:alucontrol <= `EXE_OR_OP;
+				`EXE_XOR	:alucontrol <= `EXE_XOR_OP;
+				`EXE_NOR	:alucontrol <= `EXE_NOR_OP;
+				// 移位指令
+                `EXE_SLL    :alucontrol <= `EXE_SLL_OP;
+                `EXE_SLLV   :alucontrol <= `EXE_SLLV_OP;
+                `EXE_SRL    :alucontrol <= `EXE_SRL_OP;
+                `EXE_SRLV   :alucontrol <= `EXE_SRLV_OP;
+                `EXE_SRA    :alucontrol <= `EXE_SRA_OP;
+                `EXE_SRAV   :alucontrol <= `EXE_SRAV_OP;
+                // 数据移动指令
+                `EXE_MFHI   :alucontrol <= `EXE_MFHI_OP;
+                `EXE_MTHI   :alucontrol <= `EXE_MTHI_OP;
+                `EXE_MFLO   :alucontrol <= `EXE_MFLO_OP;
+                `EXE_MTLO   :alucontrol <= `EXE_MTLO_OP;
+                // 基本运算指令
+                `EXE_ADD    :alucontrol <= `EXE_ADD_OP;
+                `EXE_ADDU   :alucontrol <= `EXE_ADDU_OP;
+                `EXE_SUB    :alucontrol <= `EXE_SUB_OP;
+                `EXE_SUBU   :alucontrol <= `EXE_SUBU_OP;
+                `EXE_SLT    :alucontrol <= `EXE_SLT_OP;
+                `EXE_SLTU   :alucontrol <= `EXE_SLTU_OP;
+                `EXE_MULT   :alucontrol <= `EXE_MULT_OP;
+                `EXE_MULTU  :alucontrol <= `EXE_MULTU_OP;
+                `EXE_DIV    :alucontrol <= `EXE_DIV_OP;
+                `EXE_DIVU   :alucontrol <= `EXE_DIVU_OP;
+                // J型跳转指令
+                `EXE_JR     :alucontrol <= `EXE_J_OP;
+                `EXE_JALR   :alucontrol <= `EXE_JALR_OP;
+				default: 
 			endcase
+			// 立即数逻辑运算指令
+            `EXE_ANDI   :alucontrol <= `EXE_ANDI_OP;
+            `EXE_XORI   :alucontrol <= `EXE_XORI_OP;
+            `EXE_LUI    :alucontrol <= `EXE_LUI_OP;
+            `EXE_ORI    :alucontrol <= `EXE_ORI_OP;
+			// 立即数运算指令
+            `EXE_ADDI   :alucontrol <= `EXE_ADDI_OP     ;
+            `EXE_ADDIU  :alucontrol <= `EXE_ADDIU_OP    ;
+            `EXE_SLTI   :alucontrol <= `EXE_SLTI_OP     ;
+            `EXE_SLTIU  :alucontrol <= `EXE_SLTIU_OP    ;
+			// J型跳转指令
+            `EXE_J      :alucontrol <= `EXE_J_OP        ;
+            `EXE_JAL    :alucontrol <= `EXE_JAL_OP      ;
+			
 		endcase
 	
 	end
