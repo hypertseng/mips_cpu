6f3d75ad9a1672a96827a85c971bc8148208b2b1
BraveSeeker <202419330@qq.com>
2023-01-03 11:17:06 +0800

    pc bug

--- rtl/pc.v
+++ rtl/pc.v
@@ -1,33 +1,33 @@
 `timescale 1ns / 1ps
 
 module pc #(parameter WIDTH = 32)(
-	input wire clk,rst,stallF,
+	input wire clk,rst,en,
 	input wire [WIDTH-1:0] pcnext,
 	output reg [WIDTH-1:0] pc,
 	output reg ce
     );
 
 
-	// always @(posedge clk) begin
-    //     if(rst) begin
-    //         ce <= 0;
-    //     end
-    //     else begin
-    //         ce <= 1;
-    //     end
-    // end
-	// always @(posedge clk) begin
-	// 	if(!ce) 
-	// 		pc <= 32'hbfc00000;
-	// 	else if(~stallF) 
-	// 		pc <= pcnext;
-		
-	// end
-	always @(posedge clk, posedge rst) begin
-		if(rst)begin
-			pc <= 0;
-		end
-		else if(~stallF)
+	always @(posedge clk) begin
+        if(rst) begin
+            ce <= 0;
+        end
+        else begin
+            ce <= 1;
+        end
+    end
+	always @(posedge clk) begin
+		if(!ce) 
+			pc <= 32'hbfc00000;
+		else if(en) 
 			pc <= pcnext;
+		
 	end
+	// always @(posedge clk, posedge rst) begin
+	// 	if(rst)begin
+	// 		pc <= 0;
+	// 	end
+	// 	else if(~stallF)
+	// 		pc <= pcnext;
+	// end
 endmodule
